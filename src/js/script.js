b21f0e2fc0b4ecee534551a85ed048a29d2ba4d0
LeonV <leonvv25@gmail.com>
2021-02-17 23:42:18 +0200

    commit_aerows

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+        speed: 1200,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
       });
   });
