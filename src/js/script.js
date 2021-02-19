e46aef354cf734494bb99689b3c3480ffc373ab7
LeonV <leonvv25@gmail.com>
2021-02-19 22:26:43 +0200

    commit_complit_3_13

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+/* Початок коду для слайдера по способу Slick Slider */
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        /* adaptiveHeight: true, */
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow:'<button type="button" class="slick-next"><img src="icons/right.png"></button>',
         responsive: [
             {
-                breakpoint: 992,/* класична точка переходу з компютерів на планшети і мобільні версії */
+                breakpoint: 992,
                 settings: {
                   dots: true,
                   arrows: false
@@ -14,4 +14,24 @@ $(document).ready(function(){
               },
         ]
       });
-  });
+  }); */
+/* Кінець коду для слайдера по способу Slick Slider */
+
+/* Початок коду для слайдера по способу Tiny Slider */
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
+ /* Кінець коду для слайдера по способу Tiny Slider */
