56b90dc99a6093a9254daebfcf074cf9360846a6
LeonV <leonvv25@gmail.com>
2021-02-28 22:59:03 +0200

    commit_3.15_21:11

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,33 @@
 /* Початок коду для слайдера по способу Slick Slider */
-/* $(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="icons/right.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-              },
-        ]
-      });
-  }); */
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    speed: 1200,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+    nextArrow:'<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
+
+
+});
 /* Кінець коду для слайдера по способу Slick Slider */
 
 /* Початок коду для слайдера по способу Tiny Slider */
-const slider = tns({
+/* const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
@@ -33,5 +42,5 @@ document.querySelector('.prev').addEventListener('click', function () {
 
 document.querySelector('.next').addEventListener('click', function () {
   slider.goTo('next');
-});
+}); */
  /* Кінець коду для слайдера по способу Tiny Slider */
