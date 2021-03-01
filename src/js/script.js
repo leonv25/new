a507f5738304b6097ca45483c8413e39dc1fe896
LeonV <leonvv25@gmail.com>
2021-03-02 01:31:31 +0200

    commit_3_15_compl

--- src/js/script.js
+++ src/js/script.js
@@ -14,17 +14,55 @@ $(document).ready(function(){
       }
     ]
   });
+  /* Кінець коду для слайдера по способу Slick Slider */
 
+  /* початок коду для табів */
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {/* потрібна крапка */
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active') /* не потрібна крапка */
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
+  /* кінець коду для табів */
+
+  /* початок коду для переключання інформації по посиланнях - не оптимізований */
+  /* $('.catalog-item__link').each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+  });
+
+  $('.catalog-item__back').each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+  }); */
+  /* кінець коду для переключання інформації по посиланнях - не оптимізований */
+
+   /* початок коду для переключання інформації по посиланнях - оптимізований */
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+   /* кінець коду для переключання інформації по посиланнях - оптимізований */
+
+
 
 
 
 });
-/* Кінець коду для слайдера по способу Slick Slider */
+
 
 /* Початок коду для слайдера по способу Tiny Slider */
 /* const slider = tns({
